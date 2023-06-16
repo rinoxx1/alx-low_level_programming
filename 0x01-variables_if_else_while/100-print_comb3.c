d2529b1c257e51dbe29e4fc3da70f05553ad40e4
rinoxx1 <mohamed3boughda@gmail.com>
2023-06-16 08:40:08 -0700

    c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+
+int main(void)
+{
+	int d1 = 0, d2;
+
+	while (d1 <= 9)
+	{
+		d2 = 0;
+		while (d2 <= 9)
+		{
+			if (d1 != d2 && d1 < d2)
+			{
+				putchar(d1 + 48);
+				putchar(d2 + 48);
+				if (d1 + d2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			d2++;
+		}
+		d1++;
+	}
+	putchar('\n');
+	return (0);
+}
