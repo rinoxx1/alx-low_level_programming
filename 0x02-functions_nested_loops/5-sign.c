3a97c88fd62dddc439f6bb97323190ca59b381d4
rinoxx1 <mohamed3boughda@gmail.com>
2023-06-20 13:43:00 -0700

    functions

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_sign - check if number is positive or negative
+ *			or 0
+ *
+ * @n: takes integer type input for function
+ *
+ * Return: 1 if +, 0 if 0 and -1 if -
+*/
+
+int print_sign(int n)
+{
+	int n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
