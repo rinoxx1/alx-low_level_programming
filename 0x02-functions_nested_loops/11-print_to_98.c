12cabed0c78a5121da5cbbf0f67454e7d97e40fe
rinoxx1 <mohamed3boughda@gmail.com>
2023-06-20 14:59:33 -0700

    functions

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,12 +10,12 @@ void print_to_98(int n)
 {
 	int count;
 
-	if (n > 89)
+	if (n > 98)
 		for (count = n; count > 98; count--)
 			printf("%d. ", count);
 	else
 		for (count = n; count < 98; count++)
 			printf("%d. ", count);
-	printf("89\n");
+	printf("98\n");
 
 }
