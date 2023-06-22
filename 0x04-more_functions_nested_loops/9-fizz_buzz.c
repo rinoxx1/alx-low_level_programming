2ce60dc9913a68d6090fa2e558668b5d321320a5
rinoxx1 <mohamed3boughda@gmail.com>
2023-06-22 09:29:49 -0700

    fonctions

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main()
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+			printf("Fizz");
+		else if (i % 5 == 0 && i % 3 != 0)
+			printf("Buzz");
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", i);
+		printf(" ");
+	}
+	return (0);
+}
