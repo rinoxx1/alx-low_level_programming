dd8bda2576b457d304de404e3ab9f8ea847640e0
rinoxx1 <mohamed3boughda@gmail.com>
2023-06-21 21:33:01 -0700

    more functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiply
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: return result
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers exept 2 and 4
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		if (n == 2 || num == 4)
+			continue;
+		_putchar(n);
+	}
+	_putchar('\n');
+}
