6bdfcc4dbde458904effbc8a79a94b249e8ce084
rinoxx1 <mohamed3boughda@gmail.com>
2023-06-22 09:11:41 -0700

    fonctions

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,10 +16,14 @@ void print_diagonal(int n)
 
 		for (i = 0; i < n; i++)
 		{
-			_putchar('\\');
-			for (j = 0; j < i; j++)
-				_putchar(' ');
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ')
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
