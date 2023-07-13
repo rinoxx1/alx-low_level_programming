3733aaa4fa746a8ee4739d63a7433666ff3566c1
rinoxx1 <mohamed3boughda@gmail.com>
2023-07-13 12:45:04 +0100

    malloc, free

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,20 @@
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * *malloc_checked - allocates memory using malloc and exit if failed
- * @b: int
- * Return: pointer to array initialized or NULL
-*/
-
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: a pointer to the allocated memory
+ */
 void *malloc_checked(unsigned int b)
 {
-	int *m = malloc(b);
+	void *ptr;
 
-	if (m == 0)
+	ptr = malloc(b);
+
+	if (ptr == NULL)
 		exit(98);
-	return (m);
+
+	return (ptr);
 }
--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,40 +1,46 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
+
 /**
  * *_memset - fills memory with a constant byte
- * @s: pointer to put the constant
- * @b: constant
- * @n: max bytes to use
- * Return: s
-*/
-
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ *
+ * Return: pointer to the memory area s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
-	char *ptr = s;
+	unsigned int i;
 
-	while (n--)
-		*s++ = b;
-	return (ptr);
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
 }
 
 /**
- * *_calloc - allocated memory for an array, using malloc
- * @nmemb: array length
+ * *_calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
  * @size: size of each element
- * Return: pointer
-*/
-
+ *
+ * Return: pointer to allocated memory
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *m;
+	char *ptr;
 
-	if (size == 0 || nmemb == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
-	m = malloc(sizeof(int) * nmemb);
 
-	if (m == NULL)
+	ptr = malloc(size * nmemb);
+
+	if (ptr == NULL)
 		return (NULL);
-	_memset(m, 0, sizeof(int) * nmemb);
 
-	return (m);
+	_memset(ptr, 0, nmemb * size);
+
+	return (ptr);
 }
