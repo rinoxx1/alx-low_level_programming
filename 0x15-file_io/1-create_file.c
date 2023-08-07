3380d27f0a98b4145ca71c05e1d4b54cb385e438
rinoxx1 <mohamed3boughda@gmail.com>
2023-08-07 13:17:01 +0100

    io

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length
+ * @s: the string
+ *
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int j = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		j++;
+	return (j);
+}
+/**
+ * create_file - create a file
+ * @filename: name of file to create
+ * @text_content: text to write
+ *
+ * Return: 1 on success and 0 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int op;
+	ssize_t bytes = 0, len = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+	op = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (len)
+		bytes = write(op, text_content, len);
+	close(fd);
+	return (bytes == len ? 1 : -1);
+}
