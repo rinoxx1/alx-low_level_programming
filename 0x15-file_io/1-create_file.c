ca0ea641f73cf439ce4aec6967762bd94d5b7173
rinoxx1 <mohamed3boughda@gmail.com>
2023-08-07 13:20:43 +0100

    io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -33,7 +33,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	op = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
+	if (op == -1)
 		return (-1);
 	if (len)
 		bytes = write(op, text_content, len);
