7d9c314295dac1016805bfd1e0e982508c67dd13
rinoxx1 <mohamed3boughda@gmail.com>
2024-01-05 18:02:42 +0100

    files

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,10 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 char *create_buffer(char *file);
 void close_file(int fd);
-
 /**
  * creates_buffer - Allocates 1024 bytes for a buffer.
  * @file: The name of the file buffer is storing chars for.
@@ -16,17 +14,14 @@ char *creates_buffer(char *file)
 	char *buffer;
 
 	buffer = malloc(sizeof(char) * 1024);
-
 	if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO,
 			"Error: Can't write to %s\n", file);
 		exit(99);
 	}
-
 	return (buffer);
 }
-
 /**
  * close_file - Closes file descriptors.
  * @fd: The file descriptor to be closed.
@@ -36,14 +31,12 @@ void close_file(int fd)
 	int c;
 
 	c = close(fd);
-
 	if (c == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
-
 /**
  * main - Copies the contents of a file to another file.
  * @argc: The number of arguments supplied to the program.
@@ -66,12 +59,10 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	buffer = create_buffer(argv[2]);
 	from = open(argv[1], O_RDONLY);
 	r = read(from, buffer, 1024);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
 	do {
 		if (from == -1 || r == -1)
 		{
@@ -80,7 +71,6 @@ int main(int argc, char *argv[])
 			free(buffer);
 			exit(98);
 		}
-
 		w = write(to, buffer, r);
 		if (to == -1 || w == -1)
 		{
@@ -89,16 +79,12 @@ int main(int argc, char *argv[])
 			free(buffer);
 			exit(99);
 		}
-
 		r = read(from, buffer, 1024);
 		to = open(argv[2], O_WRONLY | O_APPEND);
-
 	} while (r > 0);
-
 	free(buffer);
 	close_file(from);
 	close_file(to);
-
 	return (0);
 }
 
